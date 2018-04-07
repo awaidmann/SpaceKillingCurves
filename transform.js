16d0230be285e4d526a407e1502b68b0632c96a7
Alan Waidmann <alan.waidmann@gmail.com>
2018-04-06 23:20:21 -0700

    Prevent normalization functions from clobbering additional data on point object

--- transform.js
+++ transform.js
@@ -1,7 +1,7 @@
 function normalize(p, t) {
-  return t ? { x: (p.x - t.x)/t.k, y: (p.y - t.y)/t.k } : p
+  return t ? Object.assign({}, p, { x: (p.x - t.x)/t.k, y: (p.y - t.y)/t.k }) : p
 }
 
 function invertNormalize(p, t) {
-  return t ? { x: (p.x*t.k + t.x), y: (p.y*t.k + t.y) } : p
+  return t ? Object.assign({}, p, { x: (p.x*t.k + t.x), y: (p.y*t.k + t.y) }) : p
 }
