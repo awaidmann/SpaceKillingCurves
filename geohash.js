3dc8e145b4367b8c4af9e4e1b610472b676e43c3
Alan Waidmann <alan.waidmann@gmail.com>
2018-04-06 21:02:36 -0700

    Initial Hilbert Curve implementation (quadrantRangesForSearch not implemented as I don't have an efficient algorithm for it)

--- geohash.js
+++ geohash.js
@@ -169,3 +169,38 @@ Morton.prototype.quadrantRangesForSearch = function (viewRect, maxRanges) {
 
   return Morton._combineQuadTrees(xSearchTree, ySearchTree)
 }
+
+var Hilbert = function(geoHash, origin) {
+  Morton.call(this, geoHash, origin)
+}
+
+// adapted from: http://blog.notdot.net/2009/11/Damn-Cool-Algorithms-Spatial-indexing-with-Quadtrees-and-Hilbert-Curves
+// [<Configuation index> [<Z-order quad>  [<Hilbert value>, <Next Configuation index>]]]
+Hilbert._ROTATION_MAP = {
+  "a": [[[0, 0], "b"], [[0, 1], "a"], [[1, 1], "c"], [[1, 0], "a"]],
+  "b": [[[0, 0], "a"], [[1, 1], "d"], [[0, 1], "b"], [[1, 0], "b"]],
+  "c": [[[1, 0], "c"], [[0, 1], "c"], [[1, 1], "a"], [[0, 0], "d"]],
+  "d": [[[1, 0], "d"], [[1, 1], "b"], [[0, 1], "d"], [[0, 0], "c"]],
+}
+
+Hilbert.prototype = Object.create(Morton.prototype)
+Hilbert.prototype.constructor = Hilbert
+
+Hilbert.prototype.quadrantForPoint = function (point, threshold) {
+  function hilbertFromMortonQuad(zOrder, parentQuad) {
+    if (zOrder.length >= 2) {
+      var quadSlice = zOrder.slice(0, 2)
+      var quadNumeral = (quadSlice[0] << 1) | quadSlice[1]
+
+      var nextQuadMap = Hilbert._ROTATION_MAP[parentQuad][quadNumeral]
+      return nextQuadMap[0].concat(hilbertFromMortonQuad(zOrder.slice(2), nextQuadMap[1]))
+    }
+
+    return []
+  }
+
+  // call super type (Morton) method and then transform Morton encoding to Hilbert encoding
+  var zOrderQuad = Morton.prototype.quadrantForPoint.call(this, point, threshold)
+  return zOrderQuad ? hilbertFromMortonQuad(zOrderQuad, "b") : undefined
+}
+Hilbert.prototype.quadrantRangesForSearch = function (viewRect, maxRanges) {}
