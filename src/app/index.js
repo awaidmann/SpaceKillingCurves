25a30e088e5883b225d4750375848946dbcea7b3
Alan Waidmann <alan.waidmann@gmail.com>
2018-06-25 20:56:40 -0700

    Use all Bootstrap styles and fonts

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import ResizeableApp from './containers/ResizeableApp'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './style/main.css'
 
 render(
