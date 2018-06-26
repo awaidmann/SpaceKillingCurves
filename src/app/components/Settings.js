77e9af38d113741475f7d76570395b5813c341bf
Alan Waidmann <alan.waidmann@gmail.com>
2018-06-25 21:21:39 -0700

    Style tile visibility toggle

--- src/app/components/Settings.js
+++ src/app/components/Settings.js
@@ -14,14 +14,16 @@ export default class Settings extends React.Component {
   }
 
   render() {
+    const isActive = this.props.settings[TILES_VISIBLE]
     return (
       <div>
-        <label>
-          <button type="button" className="" onClick={this.toggleTilesVisible}>
-            { this.props.settings[TILES_VISIBLE] ? 'On' : 'Off' }
-          </button>
-          <span>Show tiles</span>
-        </label>
+        <button
+          type="button"
+          className={'btn ' + (isActive ? 'btn-primary' : 'btn-secondary') + ' btn-sm btn-block'}
+          onClick={this.toggleTilesVisible}
+        >
+          { isActive ? 'Hide tiles' : 'Show tiles' }
+        </button>
       </div>
     )
   }
