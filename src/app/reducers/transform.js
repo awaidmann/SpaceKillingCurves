39685d5ffd86d3418cb257719316f7fc2e37374e
Alan Waidmann <alan.waidmann@gmail.com>
2018-06-29 00:08:14 -0700

    Set initial transform to be the identity transform constant

--- src/app/reducers/transform.js
+++ src/app/reducers/transform.js
@@ -1,10 +1,7 @@
+import { zoomIdentity } from 'd3-zoom'
 import { TRANSFORM } from '../actions/transform'
 
-const INITIAL_STATE = {
-  x: 0,
-  y: 0,
-  k: 1,
-}
+const INITIAL_STATE = zoomIdentity
 
 function transform(state = INITIAL_STATE, action) {
   switch(action.type) {
